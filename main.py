93b4828d6ac7fa856010da6081c7aeae1004e481
Ashu Yadav <ashuyadav4250@gmail.com>
2022-02-11 18:41:30 +0530

    Added functions and multiple image save feature

--- main.py
+++ main.py
@@ -2,7 +2,18 @@ from ast import Import
 import pyqrcode
 import png
 from pyqrcode import QRCode
+import random, secrets
 
-link = "https://www.wikipedia.com"
-url = pyqrcode.create(link)
-url.png("qrcode.png", scale=6)
+
+def qrstart():
+    print("Hello There\nThis is QR Image Generator.\nEnter any link to create QR Code")
+    link = input("Link: ")
+    
+    def qr_generate():
+        url = pyqrcode.create(link)
+        qr_img = f"qrcodes/{secrets.token_urlsafe(6)}.png"
+        url.png(qr_img, scale=6)
+        print(f"You're QR Image is saved in qrcodes folder")
+    qr_generate()
+
+qrstart()
